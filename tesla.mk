52023e8805b74b7d5d5d66d881090f31dcade18f
zlaja <zlixmix@gmail.com>
2015-09-25 21:01:50 +0200

    Tesla, the God of lightning

--- tesla.mk
+++ tesla.mk
@@ -1,5 +1,5 @@
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Tesla stuff.
+$(call inherit-product, vendor/tesla/config/common_full_phone.mk)
 
 # Boot animation
 TARGET_SCREEN_WIDTH := 480
@@ -7,6 +7,6 @@ TARGET_SCREEN_HEIGHT := 800
 
 # Release name
 PRODUCT_RELEASE_NAME := LG L70
-PRODUCT_NAME := cm_w5
+PRODUCT_NAME := tesla_w5
 
 $(call inherit-product, device/lge/w5/full_w5.mk)
